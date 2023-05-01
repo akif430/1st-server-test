6d6377394b7655ce90bd5e5c5185fc38ba55b143
akif430 <naqiudinakif@rocketmail.com>
2023-05-01 12:32:24 +0800

    first
    try

--- server.js/server.js
+++ server.js/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('test test kali');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(process.env.PORT||3000);
