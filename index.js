7b4e14932e534937b34f6fd80798becc2a838b56
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-05-08 12:21:06 +0800

    add save and fetch request

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+process.env.CYCLIC_DB = 'bewildered-galoshes-eelCyclicDB'
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const table = CyclicDB("bewildered-galoshes-eelCyclicDB")
+let wtl = table.collection("waterLevel")
+
+app.use(express.json())
+app.set("view engine", "ejs")
+
+app.all('/', (req, res) => res.render("index.ejs"))
+
+app.post("/save", async (req, res) => {
+    if (req.body === undefined || req.body === {}) {
+        res.status(500)
+        return
+    }
+    // dateTime
+    // water level
+    // condition
+    let result = await wtl.set(req.body.dateTime, {
+        waterLevel: req.body.waterLevel,
+        condition: req.body.condition
+    })
+    res.send("success");
 })
+
+app.get('/fetch', async (req, res) => {
+    let list = await wtl.filter({})
+    console.log(list);
+    res.send(list)
+})
+
 app.listen(process.env.PORT || 3000)
