790eee5a53f07ccf64e2a6fe3168286c94fe5619
akif430 <naqiudinakif@rocketmail.com>
2023-05-15 11:24:10 +0800

    push 15/5

--- index.js
+++ index.js
@@ -12,23 +12,40 @@ app.set("view engine", "ejs")
 app.all('/', (req, res) => res.render("index.ejs"))
 
 app.post("/save", async (req, res) => {
+
+    // console.log(req.params)
+
     if (req.body === undefined || req.body === {}) {
         res.status(500)
         return
     }
+
+    console.log(`Data Received ${req.body}`)
+    console.log(req.body)
     // dateTime
     // water level
     // condition
-    let result = await wtl.set(req.body.dateTime, {
-        waterLevel: req.body.waterLevel,
-        condition: req.body.condition
-    })
-    res.send("success");
+    // if (req.body !== undefined){
+    //     let result = await wtl.set(req.body.dateTime, {
+    //         waterLevel: req.body.waterLevel,
+    //         condition: req.body.condition
+    //     })
+    // }
+    
+    // res.send("success");
+    res.status(200).json({
+        status: 'success'
+    });
 })
+
+
+
+
 
 app.get('/fetch', async (req, res) => {
     let list = await wtl.filter({})
     console.log(list);
     res.send(list)
+    
 })
 app.listen(process.env.PORT || 3000)
