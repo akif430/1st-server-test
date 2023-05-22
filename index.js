62a37d611c8ff5ebb3b0c50cc374b73a0317bb1a
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-05-22 17:39:32 +0800

    add new api point for parallel scan function

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const moment = require('moment')
 process.env.CYCLIC_DB = 'real-puce-llama-sariCyclicDB'
 const CyclicDB = require('@cyclic.sh/dynamodb')
-const table = CyclicDB("real-puce-llama-sariCyclicDB")
-let wtl = table.collection("waterLevel")
+const db = CyclicDB("real-puce-llama-sariCyclicDB")
+let wtl = db.collection("waterLevel")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -18,7 +18,7 @@ app.post("/save", async (req, res) => {
     }
 
     const date = moment().format('YYYY / MM / DD, HH:mm:ss');
-    
+
     let result = await wtl.set(date, {
         waterLevel: req.body.waterLevel,
         condition: "safe"
@@ -27,6 +27,24 @@ app.post("/save", async (req, res) => {
     res.status(200).json({
         status: 'success'
     });
+})
+
+app.get('/:col/:limit', async (req, res) => {
+    //example:  https://real-puce-llama-sari.cyclic.app/waterLevel/10
+    const col = req.params.col
+    const lim = Number(req.params.limit)
+    const filter = {
+        expression: undefined,
+        attr_vals: undefined,
+        attr_names: undefined
+    }
+    try {
+        const items = await db.collection(col).parallel_scan(filter, 0, 2, lim + 1)
+        items.results = await items.results.slice(1)
+        res.json(items).end()
+    } catch (error) {
+        res.sendStatus(500).end()
+    }
 })
 
 app.get('/fetch', async (req, res) => {
