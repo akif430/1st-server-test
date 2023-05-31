5f99f326c2c776945e1c0c7aa4c06a7986ac3b07
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-06-01 00:01:36 +0800

    update fetch endpoint and add new column in index.ejs
    1) fetch both waterLevel and waterTemperature collection
    2) add condition statement for water level and water temperature post endpoint
    3) add new columns

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 const moment = require('moment-timezone');
-const {initData, loadData, saveData} = require('./filesystem.js')
+const { initData, loadData, saveData } = require('./filesystem.js')
 
+//water level hit warning if below 0.5
 const WaterLevelWarning = 0.5;
+//water level hit danger if below 0.2
 const WaterLevelDanger = 0.2;
 
 const WaterTemperatureWarningCold = 10;
@@ -22,7 +24,7 @@ let wtemp = db.collection("waterTemperature")                                   
 
 let dataItemArr = initData()
 
-app.post('/savefs', async (req,res) => {
+app.post('/savefs', async (req, res) => {
     if (req.body === undefined || req.body === {}) {
         res.status(500)
         return
@@ -30,7 +32,7 @@ app.post('/savefs', async (req,res) => {
 
     const date = new Date().toLocaleString('en-ES');
     console.log(`New ${date}`);
-    dataItemArr.push({waterLevel:req.body.waterLevel,Date:date, condition:"safe"})                    //+Temperature //waterTemperature:req.body.waterTemperature,
+    dataItemArr.push({ waterLevel: req.body.waterLevel, Date: date, condition: "safe" })                    //+Temperature //waterTemperature:req.body.waterTemperature,
     console.log(dataItemArr);
     saveData(dataItemArr)
     res.status(200).json({
@@ -43,17 +45,23 @@ app.post('/save', async (req, res) => {
         res.status(500)
         return
     }
+    //condition statement
+    let condition = 'normal'
+    if(req.body.waterLevel <= WaterLevelWarning && req.body.waterLevel >  WaterLevelDanger){
+        condition = 'warning'
+    }else if(req.body.waterLevel <=  WaterLevelDanger){
+        condition = 'danger'
+    }
+
     const date = moment().tz('Asia/Kuala_Lumpur').format('YYYY / MM / DD, HH:mm:ss');
     let result = await wtl.set(date, {
         waterLevel: req.body.waterLevel,
-        condition: "safe"
-                                                                                                                    
+        condition
     })
     res.status(200).json({
         status: 'success'
     });
 })
-
 
 //waterTemperature: req.body.waterTemperature,                                                                                                  //+Temperature
 //UNTUK TEMPERATURE 
@@ -63,10 +71,15 @@ app.post('/saveTemperature', async (req, res) => {
         res.status(500)
         return
     }
+    let condition = 'danger'
+    if (req.body.waterTemperature > WaterTemperatureWarningCold && req.body.waterTemperature < WaterTemperatureWarningHot) {
+        condition = 'normal'
+    }
+    
     const date = moment().tz('Asia/Kuala_Lumpur').format('YYYY / MM / DD, HH:mm:ss');
     let result = await wtemp.set(date, {
         waterTemperature: req.body.waterTemperature,
-
+        condition
     })
     res.status(200).json({
         status: 'success'
@@ -77,16 +90,12 @@ app.post('/saveTemperature', async (req, res) => {
 
 
 
-
-
-
-
-app.get('/fs/:lim',async (req,res)=>{
+app.get('/fs/:lim', async (req, res) => {
     const lim = req.params.lim
     try {
         let items = await loadData()
-        const startIndex = items.length-lim
-        items = items.slice(startIndex<0 ? 0:startIndex , items.length)
+        const startIndex = items.length - lim
+        items = items.slice(startIndex < 0 ? 0 : startIndex, items.length)
         console.log(items);
         res.json(items).end()
     } catch (error) {
@@ -100,11 +109,18 @@ app.get('/:col/:limit', async (req, res) => {
     const col = req.params.col
     const lim = Number(req.params.limit)
     try {
-        let items = await db.collection(col).filter({})
-        items.results.sort((a, b) => (a.key < b.key ? 1 : -1))
-        items.results = items.results.slice(0, lim)
-        console.log(items);
-        res.json(items).end()
+        //fetch from waterLevel collection
+        let waterLevel = await db.collection(col).filter({})
+        waterLevel.results.sort((a, b) => (a.key < b.key ? 1 : -1))
+        waterLevel.results = waterLevel.results.slice(0, lim)
+
+        //fetch from waterTemperature collection
+        let waterTemperature = await db.collection('waterTemperature').filter({})
+        waterTemperature.results.sort((a, b) => (a.key < b.key ? 1 : -1))
+        waterTemperature.results = waterTemperature.results.slice(0, lim)
+          
+        res.json({waterLevel , waterTemperature}).end()
+
     } catch (error) {
         res.sendStatus(500).end()
     }
