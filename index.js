14371aa8952f1297c70abd94a445c89500517283
akif430 <naqiudinakif@rocketmail.com>
2023-06-24 11:19:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const TrueWaterLevelDanger = 2.0;
 const RiverDepth = 2.0;
 
 //Determine the Height of sensor above River Bank 
-const SensorDistFromRiverBank = 1.20;
+const SensorDistFromRiverBank = 1.00;
 
 //Formula 
 //  TruewaterLevel = RiverDepth + WaterLevel (fromsensor) - SensorDistFromRiverBank 
